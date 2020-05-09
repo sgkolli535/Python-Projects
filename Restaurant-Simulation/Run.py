c86e5219dd4974d84a3d3e2d52e8f317741a0cef
sgkolli535 <59209263+sgkolli535@users.noreply.github.com>
2020-05-09 12:32:25 -0700

    Update Run.py

--- Restaurant-Simulation/Run.py
+++ Restaurant-Simulation/Run.py
@@ -5,20 +5,19 @@ Run this file in order to start the restaurant simulation program
 import datetime
 import time
 
-# TODO 0: uncomment the following 3 import statements when you create these files
 from Menu import Menu
 from Waiter import Waiter
 from RestaurantHelper import RestaurantHelper
 
 
 def main():
-    RESTAURANT_NAME = "Sumi's Bistro"  # TODO 1: add your own restaurant name in the quotes
+    RESTAURANT_NAME = "Sumi's Bistro"  
     restaurantTime = datetime.datetime(2017, 5, 1, 5, 0)
 
     # Create the menu object
-    menu = Menu("menu.csv")  # TODO 2: uncomment this once the Menu class is implemented
+    menu = Menu("menu.csv")  
     # create the waiter object using the created Menu we just created
-    waiter = Waiter(menu)  # TODO 4: uncomment this one the Waiter class is implemented
+    waiter = Waiter(menu)  
     print("Welcome to " + RESTAURANT_NAME + "!")
     print(RESTAURANT_NAME + " is now open for dinner.\n")
     #
@@ -26,19 +25,19 @@ def main():
         print("\n~~~ It is currently", restaurantTime.strftime("%H:%M PM"), "~~~")
         restaurantTime += datetime.timedelta(minutes=15)
 
-        # TODO 3: uncomment the following 3 lines once the Diner class is implemented
+        
         potentialDiner = RestaurantHelper.randomDinerGenerator()
         if potentialDiner is not None:
-            print("\n" + potentialDiner.getName() + " welcome, please be seated!\n")  # we have a diner to add to the waiter's list of diners
+            print("\n" + potentialDiner.getName() + " welcome, please be seated!\n") 
 
-        # TODO 4: uncomment the following 2 lines once the Waiter class is implemented
+        
             waiter.addDiner(potentialDiner)
         waiter.advanceDiners()
         time.sleep(2)
 
     print("\n~~~ ", RESTAURANT_NAME, "is now closed. ~~~")
     # After the restaurant is closed, progress any diners until everyone has left
-    # TODO 5: uncomment the following 5 lines once the Waiter class is implemented
+    
     while waiter.getNumDiners():
         print("\n~~~ It is currently", restaurantTime.strftime("%H:%M PM"), "~~~")
         restaurantTime += datetime.timedelta(minutes=15)
